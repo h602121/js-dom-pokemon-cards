dea01a958fb57abe40a9bf0e13cfa8c761f4cf26
Øyvind Onarheim <oyvind.aasmul.onarheim@experisacademy.no>
2024-09-24 14:21:08 +0200

    core and almost extension done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
+function renderCards() {
+  const cardHTML = document.getElementById("cards");
+  cardHTML.innerHTML = "";
+  data.forEach(function (item) {
+    cardHTML.innerHTML += `<li class="card">
+    <h2 class="card--title">${
+      item.name.charAt(0).toLocaleUpperCase() + item.name.slice(1)
+    }</h2>
+    <img
+    width="256"
+    class="card--img"
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+      item.id
+    }.png"
 
-console.log(data);
+  />
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    <ul class="card--text">
+    ${returnString(item.stats)}
+    </ul>
+    <ul>
+    ${showGames(item.game_indices)}
+    </ul>
+    </li>
+    
+    `;
+  });
+}
+renderCards();
+
+function returnString(list) {
+  let string = "";
+
+  list.forEach(function (stat) {
+    string += ` <li>${
+      stat.stat.name.charAt(0).toLocaleUpperCase() + stat.stat.name.slice(1)
+    }: ${stat.base_stat}</li>`;
+  });
+
+  return string;
+}
+
+function showGames(list) {
+  let string = "";
+  list.forEach(function (item) {
+    string += `<li> ${item.version.name} </li>`;
+  });
+
+  return string;
+}
